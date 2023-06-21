1d4a0609965086d543e41079595da4eff4a423ae
VLuca997 <kulertoast997@gmail.com>
2023-06-21 02:00:40 +0200

    Human Code intro to Pari e Dispari

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -1,2 +1,6 @@
 // prompt per richiedere se l'utente vuole pari o dispari
-//
+//trasformiamo la stringa in numero
+// generiamo numeri random da 1 a 5 per il Computer
+//sommiamo i numeri del player a quello del computer
+//con una funzione capiamo se sono pari o dispari
+// dichiariamo chi ha vinto
